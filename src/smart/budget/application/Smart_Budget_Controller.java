e24ad8c7b4e873af296ddbc894f3a434e1e1e6b6
WayneDavies1414 <waynedavies1414@gmail.com>
2019-07-16 14:39:58 +0200

    Changed the Smart_Budget_Controller

--- src/smart/budget/application/Smart_Budget_Controller.java
+++ src/smart/budget/application/Smart_Budget_Controller.java
@@ -1,3 +1,8 @@
+/**
+ * Smart_Budget_Controller: Controls the whole application
+ * 
+ */
+
 package smart.budget.application;
 
 
@@ -7,9 +12,13 @@ package smart.budget.application;
  */
 public class Smart_Budget_Controller {
 
+	//main method
     public static void main(String...smart_budget_app){
+    	
+    	System.out.println("starting app");
         Smart_Budget_Model model = new Smart_Budget_Model();
-        model.addBudgetToDB();
+       
+        model.Smart_Budget_Controller();
     
     }
     
